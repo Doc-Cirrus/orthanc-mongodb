91e16ab044b064c1a2a82b748945b07bf985b1e6
MYMAIDAN <mykhailo.maidan@gmail.com>
2021-05-17 22:33:54 +0300

    Update build.sh to build orthan service

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
-
-if [$# -eq "0"]
+#!/bin/bash
+if [ $# -eq "0" ]
 then
     echo "No arguments supplied.\n"
     echo "1 - Orthanc Version\n"
@@ -11,10 +10,22 @@ PLUGIN_DIR=$(pwd)
 
 . /etc/os-release
 OS=$NAME
-if [ $OS == "Ubuntu" ] 
+if [[ $OS == "Ubuntu" ]] 
 then
-    sudo apt -y install build-essential unzip cmake make libsasl2-dev uuid-dev libssl-dev zlib1g-dev git curl
+    apt update -y
+    apt -y install build-essential unzip cmake python make libsasl2-dev uuid-dev libssl-dev zlib1g-dev git curl wget
+    CMAKE_EXE=cmake
+elif [[ $OS == "CentOS Linux" ]]
+then
+   echo "Centos system"
+    yum -y install centos-release-scl centos-release-scl-rh epel-release
+    yum -y install make devtoolset-8 libuuid-devel python openssl-devel cyrus-sasl-devel cmake3 zlib-devel gcc-c++ wget
+    CMAKE_EXE=cmake3
+else
+echo "Not CentOS or Ubuntu"
 fi
+
+echo $OS
 
 mkdir orthanc_mongodb_plugin_build
 pushd orthanc_mongodb_plugin_build
@@ -25,12 +36,12 @@ tar -zxf get.php?path=%2Forthanc%2FOrthanc-$1.tar.gz
 ROOT_DIR=$(pwd)
 mkdir plugin_build
 pushd plugin_build
-cmake -DCMAKE_CXX_FLAGS='-fPIC' -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=$2 -DORTHANC_ROOT=$ROOT_DIR/Orthanc-$1  -DBUILD_TESTS=ON  \-DAUTO_INSTALL_DEPENDENCIES=ON $PLUGIN_DIR
+$CMAKE_EXE -DCMAKE_CXX_FLAGS='-fPIC' -DCMAKE_INSTALL_PREFIX=/usr  -DCMAKE_BUILD_TYPE=$2 -DORTHANC_ROOT=$ROOT_DIR/Orthanc-$1  -DBUILD_TESTS=ON  \-DAUTO_INSTALL_DEPENDENCIES=ON $PLUGIN_DIR
 nproc --all | make -j
 
 #Build Orthanc Service 
 popd
 mkdir orthanc_service_build
 pushd orthanc_service_build
-cmake -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=$2 $ROOT_DIR/Orthanc-$1/OrthancServer/
+$CMAKE_EXE -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=$2 $ROOT_DIR/Orthanc-$1/OrthancServer/
 nproc --all | make -j
