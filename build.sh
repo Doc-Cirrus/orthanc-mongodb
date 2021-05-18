64859c486a118e5fac5d1147623850178d2fb24f
MYMAIDAN <mykhailo.maidan@gmail.com>
2021-05-18 16:39:06 +0300

    Add conan package manager

--- build.sh
+++ build.sh
@@ -13,14 +13,25 @@ OS=$NAME
 if [[ $OS == "Ubuntu" ]] 
 then
     apt update -y
-    apt -y install build-essential unzip cmake python make libsasl2-dev uuid-dev libssl-dev zlib1g-dev git curl wget
+    apt -y install build-essential unzip cmake python make libsasl2-dev uuid-dev libssl-dev zlib1g-dev git curl wget python3-pip
+    pip3 install conan
     CMAKE_EXE=cmake
+    call_cmd()
+    {
+        $1
+    }
+
 elif [[ $OS == "CentOS Linux" ]]
 then
    echo "Centos system"
     yum -y install centos-release-scl centos-release-scl-rh epel-release
-    yum -y install make devtoolset-8 libuuid-devel python openssl-devel cyrus-sasl-devel cmake3 zlib-devel gcc-c++ wget
+    yum -y install make devtoolset-8 libuuid-devel python openssl-devel cyrus-sasl-devel cmake3 zlib-devel gcc-c++ wget python3-pip
+    pip3 install conan
     CMAKE_EXE=cmake3
+    call_cmd()
+    {
+        scl enable devtoolset-8 "$1"
+    }
 else
 echo "Not CentOS or Ubuntu"
 fi
@@ -36,12 +47,13 @@ tar -zxf get.php?path=%2Forthanc%2FOrthanc-$1.tar.gz
 ROOT_DIR=$(pwd)
 mkdir plugin_build
 pushd plugin_build
-$CMAKE_EXE -DCMAKE_CXX_FLAGS='-fPIC' -DCMAKE_INSTALL_PREFIX=/usr  -DCMAKE_BUILD_TYPE=$2 -DORTHANC_ROOT=$ROOT_DIR/Orthanc-$1  -DBUILD_TESTS=ON  \-DAUTO_INSTALL_DEPENDENCIES=ON $PLUGIN_DIR
-nproc --all | make -j
+call_cmd "conan install ${PLUGIN_DIR}"
+call_cmd "$CMAKE_EXE -DCMAKE_CXX_FLAGS='-fPIC' -DCMAKE_INSTALL_PREFIX=/usr  -DCMAKE_BUILD_TYPE=$2 -DORTHANC_ROOT=$ROOT_DIR/Orthanc-$1  -DBUILD_TESTS=ON $PLUGIN_DIR"
+call_cmd "nproc --all | make -j"
 
 #Build Orthanc Service 
-popd
-mkdir orthanc_service_build
-pushd orthanc_service_build
-$CMAKE_EXE -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=$2 $ROOT_DIR/Orthanc-$1/OrthancServer/
-nproc --all | make -j
+#popd
+#mkdir orthanc_service_build
+#pushd orthanc_service_build
+#$CMAKE_EXE -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=$2 $ROOT_DIR/Orthanc-$1/OrthancServer/
+#nproc --all | make -j
